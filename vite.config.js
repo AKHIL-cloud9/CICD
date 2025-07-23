0076ca8b69f6250e497cca2f3b79bdddc64af192
AKHIL GAIKWAD <akhilgaikwad2005sbi@gmail.com>
2025-07-23 23:00:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/CICD/', // 👈 very important: use your repo name here
   plugins: [react()],
-  base: '/CICD/'
-});
+})
